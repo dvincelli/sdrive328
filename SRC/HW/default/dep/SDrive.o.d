5d999cd147241ea00a2d077e5b966cdb71687d9f
David Vincelli <dvincelli@users.noreply.github.com>
2017-02-11 14:51:02 -0500

    Update hardcoded paths to match my system

--- SRC/HW/default/dep/SDrive.o.d
+++ SRC/HW/default/dep/SDrive.o.d
@@ -1,13 +1,13 @@
 SDrive.o: ../SDrive.c /usr/lib/avr/include/avr/io.h \
  /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/inttypes.h \
  /usr/lib/gcc/avr/4.8.2/include/stdint.h /usr/lib/avr/include/stdint.h \
- /usr/lib/avr/include/avr/iom8.h /usr/lib/avr/include/avr/portpins.h \
+ /usr/lib/avr/include/avr/iom328p.h /usr/lib/avr/include/avr/portpins.h \
  /usr/lib/avr/include/avr/common.h /usr/lib/avr/include/avr/version.h \
  /usr/lib/avr/include/avr/fuse.h /usr/lib/avr/include/avr/lock.h \
  /usr/lib/avr/include/avr/interrupt.h /usr/lib/avr/include/avr/pgmspace.h \
  /usr/lib/gcc/avr/4.8.2/include/stddef.h \
  /usr/lib/avr/include/avr/eeprom.h ../avrlibdefs.h ../avrlibtypes.h \
- ../spi.h ../mmc.h ../fat.h ../sboot.h
+ ../spi.h ../mmc.h ../fat.h ../sboot.h ../delay_x.h
 
 /usr/lib/avr/include/avr/io.h:
 
@@ -19,7 +19,7 @@ SDrive.o: ../SDrive.c /usr/lib/avr/include/avr/io.h \
 
 /usr/lib/avr/include/stdint.h:
 
-/usr/lib/avr/include/avr/iom8.h:
+/usr/lib/avr/include/avr/iom328p.h:
 
 /usr/lib/avr/include/avr/portpins.h:
 
@@ -50,3 +50,5 @@ SDrive.o: ../SDrive.c /usr/lib/avr/include/avr/io.h \
 ../fat.h:
 
 ../sboot.h:
+
+../delay_x.h:
--- SRC/HW/default/dep/fat.o.d
+++ SRC/HW/default/dep/fat.o.d
@@ -2,7 +2,7 @@ fat.o: ../fat.c /usr/lib/avr/include/stdio.h \
  /usr/lib/avr/include/inttypes.h /usr/lib/gcc/avr/4.8.2/include/stdint.h \
  /usr/lib/avr/include/stdint.h /usr/lib/gcc/avr/4.8.2/include/stdarg.h \
  /usr/lib/gcc/avr/4.8.2/include/stddef.h /usr/lib/avr/include/avr/io.h \
- /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/avr/iom8.h \
+ /usr/lib/avr/include/avr/sfr_defs.h /usr/lib/avr/include/avr/iom328p.h \
  /usr/lib/avr/include/avr/portpins.h /usr/lib/avr/include/avr/common.h \
  /usr/lib/avr/include/avr/version.h /usr/lib/avr/include/avr/fuse.h \
  /usr/lib/avr/include/avr/lock.h /usr/lib/avr/include/avr/pgmspace.h \
@@ -25,7 +25,7 @@ fat.o: ../fat.c /usr/lib/avr/include/stdio.h \
 
 /usr/lib/avr/include/avr/sfr_defs.h:
 
-/usr/lib/avr/include/avr/iom8.h:
+/usr/lib/avr/include/avr/iom328p.h:
 
 /usr/lib/avr/include/avr/portpins.h:
 
